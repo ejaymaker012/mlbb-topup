fcb263655d93a878952ad1535b7f524265f1eac0
ejaymaker012 <ejaymaker012@gmail.com>
2026-06-06 22:15:39 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,6 @@ app.get('/history', auth, (req, res) => {
     }
   );
 });
-
+app.use(cors({ origin: "mlbb-topup-ejaymaker-s-projects.vercel.app" }));
 app.listen(process.env.PORT || 3000, () => console.log(`MLBB Top-Up Server running on port ${process.env.PORT || 3000}`));
 
-app.use(cors({ origin: "https://mlbb-topup.vercel.app" }));
